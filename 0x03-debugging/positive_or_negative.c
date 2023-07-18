b50d82a7ec8a77639b33d5bd65ac49493c784923
Bridget Naa lamiley Lamptey <bridgetnaalamiley@gmail.com>
2023-07-18 08:36:47 -0700

    Test postitive_or_negative() function with 0-main.c file and main.h local header

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * positive_or_negative - Entry point
+ *
+ * @i: Integer variable to be checked
+*/
+void positive_or_negative(int i)
+{
+	if (i > 0)
+		printf("%d is positive\n", i);
+	else if (i == 0)
+		printf("%d is zero\n", i);
+	else
+		printf("%d is negative\n", i);
+}
